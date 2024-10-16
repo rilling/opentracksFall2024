0f4290d45e514a06d4771dff515cb77403a846c0
Anuja Ajay Somthankar <56058617+Anuja-Somthankar@users.noreply.github.com>
2024-10-15 21:27:17 -0400

    Team 20 - Case Study 1 (#17)
    * fixed: Added override annotation in ChartFragment. Closes #2
    
    * - Removed sonarqube properties from build.gradle
    
    * Added the missing @deprecated Javadoc tag.
    
    * Re-commited the changes to my branch.
    
    * Removed the unused 'title' variable from TrackRecordedActivity.java
    
    * - Resolved the merge conflicts and errors related to build.gradle
    
    ---------
    
    Co-authored-by: kapildev333 <kapssoni333@gmail.com>
    Co-authored-by: simran <simran6520@gmail.com>
    Co-authored-by: karandeep7 <karansaini591@gmail.com>
    Co-authored-by: honey25s <honeysharma1925@gmail.com>

--- src/main/java/de/dennisguse/opentracks/chart/ChartFragment.java
+++ src/main/java/de/dennisguse/opentracks/chart/ChartFragment.java
@@ -256,9 +256,10 @@ public class ChartFragment extends Fragment implements TrackDataHub.Listener {
     /**
      * Returns true if the selected track is recording.
      * Needs to be synchronized because trackDataHub can be accessed by multiple threads.
+     *
+     * @deprecated This method should not be dynamic but instead set while instantiating, i.e., in newFragment().
      */
     @Deprecated
-    //TODO Should not be dynamic but instead set while instantiating, i.e., newFragment().
     private synchronized boolean isSelectedTrackRecording() {
         return trackDataHub != null && trackDataHub.isSelectedTrackRecording();
     }
