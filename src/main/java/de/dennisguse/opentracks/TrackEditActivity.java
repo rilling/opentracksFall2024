bc82ccfe5a32827543b3cd7e8e6009f84ef1d89c
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-11-03 23:02:41 -0500

    Setup PMD and CPD, plus generated report for clones

--- src/main/java/de/dennisguse/opentracks/TrackEditActivity.java
+++ src/main/java/de/dennisguse/opentracks/TrackEditActivity.java
@@ -62,7 +62,7 @@ public class TrackEditActivity extends AbstractActivity implements ChooseActivit
 
 
         ContentProviderUtils contentProviderUtils = new ContentProviderUtils(this);
-        Track Track track = contentProviderUtils.getTrack(trackId);
+        Track track = contentProviderUtils.getTrack(trackId);
 
         if (track == null) {
             Log.e(TAG, "No track for " + trackId.id());
